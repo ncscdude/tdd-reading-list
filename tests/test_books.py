741f153a58a51493371e620551a8cd71bf6c98e8
Robert McMillan <mcmillan.robert@gmail.com>
2026-06-10 15:57:50 -0400

    GREEN: multiple books test passing

--- tests/test_books.py
+++ tests/test_books.py
@@ -8,5 +8,13 @@ def test_new_reading_list_is_empty():
 
 def test_can_add_a_book():
     reading_list = ReadingList()
-    reading_list.add_book("The Hobbit")
-    assert reading_list.get_all_books() == ["The Hobbit"]
+    reading_list.add_book('The Hobbit')
+    assert reading_list.get_all_books() == ['The Hobbit']
+
+
+def test_can_add_multiple_books():
+    reading_list = ReadingList()
+    reading_list.add_book('The Hobbit')
+    reading_list.add_book('Dune')
+    assert reading_list.get_all_books() == ['The Hobbit', 'Dune']
+
